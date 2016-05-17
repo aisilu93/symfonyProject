329a4b94cbbb8e970004e5e6982613d052ae4a19
aisilu93 <aisilu93@yandex.ru>
2016-05-17 21:45:30 +0300

    Changes in BlogBundle/../DefaultController.php

--- src/firstspace/BlogBundle/Controller/DefaultController.php
+++ src/firstspace/BlogBundle/Controller/DefaultController.php
@@ -2,25 +2,22 @@
 
 namespace firstspace\BlogBundle\Controller;
 
+//use firstspace\BlogBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class DefaultController extends Controller
-{
+{	
 	 /**
-     * @Route("/admin")
+     * @Route("/user/{id}")
      */
-    public function indexAction()
+	public function pageAction($id)
     {
-        return $this->render('firstBundle:Default:index.html.twig');
-    }
-	
-	public function adminAction()
-    {
-		$roles = $this->get('first.security.roles')->getRoles();
-		print_r($roles);
-        return new Response('<html><body>Admin page!</body></html>');
+		$user = $this -> getDoctrine()
+			->getRepository('BlogBundle:User')
+			->find($id);
+			return new Response('<html><body>Admin page!</body></html>');
     }
 }
