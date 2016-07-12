ff5d55213f5a71f7f2d43523caea03308be71a13
aisilu93 <aisilu93@yandex.ru>
2016-07-12 23:47:18 +0300

    css-file has been linked with the template
    Twig template of the blog has been linked with the stylesheet. Now at
    the request page we can see only one of few posts from blog's database

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new firstspace\BlogBundle\firstBundle(),
 			new FOS\UserBundle\FOSUserBundle(),
             new CarBlogBundle\CarBlogBundle(),
+			new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
